48ac86df251094eedcb3d2a8b9ee21a939cb6dff
Micah N Gorrell <m@minego.net>
2022-02-10 10:47:58 -0700

    Rewrote to handle the state of all modifiers in a more sane fashion

--- mackeys.c
+++ mackeys.c
@@ -22,25 +22,35 @@ typedef enum {
 	KEY_STROKE_REPEAT		= 2,
 } KEY_STROKE_ENUM;
 
+typedef enum {
+	MOD_L_ALT				= 1 << 0,
+	MOD_L_CTRL				= 1 << 1,
+	MOD_L_META				= 1 << 2,
+	MOD_L_SHIFT				= 1 << 3,
+
+	MOD_R_ALT				= 1 << 4,
+	MOD_R_CTRL				= 1 << 5,
+	MOD_R_META				= 1 << 6,
+	MOD_R_SHIFT				= 1 << 7,
+} MODS;
+
 typedef struct input_event input_event;
 
 const struct input_event	syn	= { .type = EV_SYN, .code = SYN_REPORT, .value = 0 };
 
 /* Global options */
 int delay = 20000;
-BOOL swap = FALSE;
 
 static void usage(const char *program)
 {
     fprintf(stderr,
-			"%s - Use macOS style copy and paste shortcuts on Linux\n"
+			"%s - Map macOS copy, paste and cut events to ctrl+insert, shift+insert and shift+delete\n"
 			"\n"
 			"usage %s [-h | [-d delay]]\n"
 			"\n"
 			"options:\n"
 			"   -h          Show this message and exit\n"
 			"   -d          Delay used for key sequences (default: 20000 microseconds)\n"
-			"   -s          Swap alt and super keys to more closely match the layout on a mac\n"
 
 			, program, program);
 }
@@ -65,27 +75,113 @@ static void fake_event(unsigned short code, KEY_STROKE_ENUM value)
 	fake.value		= value;
 
 	write_event(&fake);
+
+	write_event(&syn);
+	usleep(delay);
+}
+
+static unsigned char which_mod(input_event *event)
+{
+	if (event->type != EV_KEY) {
+		return 0;
+	}
+
+	/* Is this event for a modifier? */
+	switch (event->code) {
+		case KEY_LEFTALT:		return MOD_L_ALT;
+		case KEY_LEFTCTRL:		return MOD_L_CTRL;
+		case KEY_LEFTMETA:		return MOD_L_META;
+		case KEY_LEFTSHIFT:		return MOD_L_SHIFT;
+
+		case KEY_RIGHTALT:		return MOD_R_ALT;
+		case KEY_RIGHTCTRL:		return MOD_R_CTRL;
+		case KEY_RIGHTMETA:		return MOD_R_META;
+		case KEY_RIGHTSHIFT:	return MOD_R_SHIFT;
+
+		default:				return 0;
+	}
+}
+
+static int which_key(unsigned char cur)
+{
+	switch (cur) {
+		case MOD_L_ALT:			return KEY_LEFTALT;
+		case MOD_L_CTRL:		return KEY_LEFTCTRL;
+		case MOD_L_META:		return KEY_LEFTMETA;
+		case MOD_L_SHIFT:		return KEY_LEFTSHIFT;
+
+		case MOD_R_ALT:			return KEY_RIGHTALT;
+		case MOD_R_CTRL:		return KEY_RIGHTCTRL;
+		case MOD_R_META:		return KEY_RIGHTMETA;
+		case MOD_R_SHIFT:		return KEY_RIGHTSHIFT;
+
+		default:				return 0;
+	}
+}
+
+/* Update the bitfield mods to reflect the state after the specified event */
+static void update_mods(unsigned char *mods, input_event *event)
+{
+	unsigned char cur;
+
+	/* Is this event for a modifier? */
+	cur = which_mod(event);
+
+	if (cur != 0) {
+		switch (event->value) {
+			case KEY_STROKE_UP:
+				(*mods) &= ~cur;
+				break;
+
+			case KEY_STROKE_DOWN:
+				(*mods) |= cur;
+				break;
+		}
+	}
+}
+
+/*
+	Send down and up events to make the current mod state match the desired
+	mod state.
+*/
+static void apply_mods(unsigned char *current_mods, unsigned char desired_mods)
+{
+	int bit;
+	unsigned char cur;
+
+	for (bit = 0; bit < 8; bit++) {
+		cur = 1 << bit;
+
+		if (((*current_mods) & cur) == (desired_mods & cur)) {
+			/* This mod is already in the needed state */
+			continue;
+		}
+
+		if ((desired_mods & cur)) {
+			fake_event(which_key(cur), KEY_STROKE_DOWN);
+			(*current_mods) |= cur;
+		} else {
+			fake_event(which_key(cur), KEY_STROKE_UP);
+			(*current_mods) &= ~cur;
+		}
+	}
 }
 
 int main(int argc, char **argv)
 {
 	char				o;
 	input_event			event;
-	BOOL				left_super_held		= FALSE;
-	BOOL				right_super_held	= FALSE;
-	BOOL				sent_super_down		= FALSE;
-	BOOL				did_modify			= FALSE;
-	BOOL				*current;
+	unsigned char		cur;
+	int					faked		= 0;
+	unsigned char		mods_bk		= 0;
+	unsigned char		mods_in		= 0;
+	unsigned char		mods_out	= 0;
 
-	while (-1 != (o = getopt(argc, argv, "htTsk:c:"))) {
+	while (-1 != (o = getopt(argc, argv, "hd:"))) {
         switch (o) {
             case 'h':
                 usage(argv[0]);
 				return EXIT_SUCCESS;
-
-			case 's':
-				swap = TRUE;
-                break;
 
 			case 'd':
 				delay = atoi(optarg);
@@ -107,151 +203,131 @@ int main(int argc, char **argv)
             continue;
         }
 
-		if (swap) {
-			switch (event.code) {
-				case KEY_LEFTMETA:	event.code = KEY_LEFTALT;	break;
-				case KEY_LEFTALT:	event.code = KEY_LEFTMETA;	break;
-				case KEY_RIGHTMETA:	event.code = KEY_RIGHTALT;	break;
-				case KEY_RIGHTALT:	event.code = KEY_RIGHTMETA;	break;
+		cur = which_mod(&event);
 
-				default:
-					break;
-			}
-		}
+		/* Ensure that mods_in contains the state as we've read them */
+		update_mods(&mods_in, &event);
 
 		switch (event.code) {
 			case KEY_LEFTMETA:
-				current = &left_super_held;
+			case KEY_RIGHTMETA:
+				switch (event.value) {
+					case KEY_STROKE_UP:
+						/*
+							If we didn't send any faked copy/paste events and
+							the meta keystroke was eaten then it should be sent
+							now to allow taps.
+						*/
+						mods_bk = mods_in;
+						mods_bk |= cur;
+
+						if (faked == 0 && mods_bk != mods_out) {
+							apply_mods(&mods_out, mods_bk);
+						}
+
+					case KEY_STROKE_REPEAT:
+						/*
+							If we have sent the down event then let this event
+							through. Otherwise eat it.
+						*/
+						if (mods_out & cur) {
+							break;
+						} else {
+							continue;
+						}
+
+					case KEY_STROKE_DOWN:
+						/*
+							Eat the down event for the meta keys, until we know
+							that this is not a meta+x|c|v keystroke.
+						*/
+						faked = 0;
+						continue;
+				}
 				break;
 
-			case KEY_RIGHTMETA:
-				current = &right_super_held;
+			case KEY_X:
+			case KEY_C:
+			case KEY_V:
+				if ((mods_in & (MOD_L_META | MOD_R_META)) == 0) {
+					/* If a meta key isn't being held then these keys aren't modified */
+					break;
+				}
+
+				if (event.value != KEY_STROKE_DOWN) {
+					/* The event is triggered on the key down only */
+					continue;
+				}
+
+				/*
+					Keep track of how many faked copy/paste events we've applied
+					so that we know if the meta key was tapped or not.
+				*/
+				faked++;
+
+				/* Remember what the state was previously */
+				mods_bk = mods_out;
+
+				/* Apply the desired mods for this faked event */
+				switch (event.code) {
+					case KEY_C:
+						apply_mods(&mods_out, MOD_L_CTRL);
+						break;
+
+					case KEY_X:
+					case KEY_V:
+						apply_mods(&mods_out, MOD_L_SHIFT);
+						break;
+				}
+
+				/* Send the actual key now that the mods are in the right state */
+				switch (event.code) {
+					case KEY_X:
+						fake_event(KEY_DELETE, KEY_STROKE_DOWN);
+						fake_event(KEY_DELETE, KEY_STROKE_UP);
+						break;
+
+					case KEY_C:
+					case KEY_V:
+						fake_event(KEY_INSERT, KEY_STROKE_DOWN);
+						fake_event(KEY_INSERT, KEY_STROKE_UP);
+						break;
+				}
+
+				/* Restore the previous modifier state */
+				apply_mods(&mods_out, mods_bk);
+
+				/* Eat the original event */
+				continue;
+
+			case KEY_LEFTALT:
+			case KEY_LEFTCTRL:
+			case KEY_LEFTSHIFT:
+			case KEY_RIGHTALT:
+			case KEY_RIGHTCTRL:
+			case KEY_RIGHTSHIFT:
+				/*
+					All mods other than meta should be sent normally, but should
+					not fall into the default case which could send an extra
+					meta down event.
+				*/
 				break;
 
 			default:
-				current = NULL;
+				/*
+					If a down event for a meta key was previously eaten then
+					send it now.
+				*/
+				if (mods_in != mods_out) {
+					apply_mods(&mods_out, mods_in);
+				}
 				break;
 		}
 
-		if (current != NULL) {
-			switch (event.value) {
-				case KEY_STROKE_UP:
-					*current = FALSE;
 
-					if (!did_modify && !sent_super_down) {
-						/*
-							It looks like the user just tapped the key which
-							is useful and should be let through.
-						*/
-						sent_super_down = TRUE;
-
-						if (left_super_held) {
-							fake_event(KEY_LEFTMETA, KEY_STROKE_DOWN);
-						}
-						if (right_super_held) {
-							fake_event(KEY_RIGHTMETA, KEY_STROKE_DOWN);
-						}
-
-						write_event(&syn);
-						usleep(delay);
-					}
-
-					if (!sent_super_down) {
-						/* We never sent the down event so don't send the up */
-						continue;
-					}
-
-					/* Reset our state */
-					did_modify = FALSE;
-					sent_super_down = FALSE;
-					break;
-
-				case KEY_STROKE_DOWN:
-					*current = TRUE;
-
-					/* Eat the event for now */
-					sent_super_down = FALSE;
-					did_modify = FALSE;
-					continue;
-			}
-		}
-
-		if (left_super_held || right_super_held) {
-			switch (event.code) {
-				default:
-					break;
-
-				case KEY_C:
-				case KEY_V:
-					if (event.value != KEY_STROKE_DOWN) {
-						/* Eat this event, we're acting on the down only */
-						continue;
-					}
-
-					/*
-						Trigger events to release the super key(s) and then send
-						ctrl+insert or shift+insert.
-
-						Trigger a ctrl+insert or a shift+insert
-
-						Send an event to release the super key(s) and then to press
-						the desired modifiers before passing through the real key.
-					*/
-					if (left_super_held) {
-						fake_event(KEY_LEFTMETA, KEY_STROKE_UP);
-					}
-					if (right_super_held) {
-						fake_event(KEY_RIGHTMETA, KEY_STROKE_UP);
-					}
-
-					switch (event.code) {
-						case KEY_C:
-							fake_event(KEY_LEFTCTRL, KEY_STROKE_DOWN);
-							break;
-						case KEY_V:
-							fake_event(KEY_LEFTSHIFT, KEY_STROKE_DOWN);
-							break;
-					}
-
-					write_event(&syn);
-					usleep(delay);
-
-					fake_event(KEY_INSERT, KEY_STROKE_DOWN);
-					write_event(&syn);
-					usleep(delay);
-
-					/* Undo everything we just did */
-
-					fake_event(KEY_INSERT, KEY_STROKE_UP);
-					usleep(delay);
-
-					switch (event.code) {
-						case KEY_C:
-							fake_event(KEY_LEFTCTRL, KEY_STROKE_UP);
-							break;
-						case KEY_V:
-							fake_event(KEY_LEFTSHIFT, KEY_STROKE_UP);
-							break;
-					}
-
-					write_event(&syn);
-					usleep(delay);
-
-					if (left_super_held) {
-						fake_event(KEY_LEFTMETA, KEY_STROKE_DOWN);
-					}
-					if (right_super_held) {
-						fake_event(KEY_RIGHTMETA, KEY_STROKE_DOWN);
-					}
-
-					write_event(&syn);
-					usleep(delay);
-
-					continue;
-			}
-		}
-
+		/* Write the event, and update mods_out to match the state we've written */
 		write_event(&event);
-    }
+		update_mods(&mods_out, &event);
+	}
 }
 
