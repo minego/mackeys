b4a8531021d72c2cdbc9868d7e9d5c6ebdf4f729
Micah N Gorrell <m@minego.net>
2023-12-02 16:23:00 -0700

    Allow building on arm as well

--- flake.nix
+++ flake.nix
@@ -2,23 +2,19 @@
 	description			= "A flake for building my mackeys plugin for interception-tools";
 	inputs.nixpkgs.url	= "github:NixOS/nixpkgs/nixos-unstable";
 
-	outputs = { self, nixpkgs }: {
+	outputs = { self, nixpkgs, ... }:
+	{
 		overlay = self: super: {
 			minego = (super.minego or {}) // {
 				mackeys = super.pkgs.callPackage ./derivation.nix { };
 			};
 		};
 
-		packages = {
-			"x86_64-linux" = rec {
-				mackeys = nixpkgs.legacyPackages."x86_64-linux".callPackage ./derivation.nix { };
-				default = mackeys;
-			};
-			"aarch64-linux" = rec {
-				mackeys = nixpkgs.legacyPackages."aarch64-linux".callPackage ./derivation.nix { };
-				default = mackeys;
-			};
-		};
+		packages.x86_64-linux.mackeys = nixpkgs.legacyPackages.x86_64-linux.callPackage ./derivation.nix {};
+		packages.x86_64-linux.default = self.packages.x86_64-linux.mackeys;
+
+		packages.aarch64-linux.mackeys = nixpkgs.legacyPackages.aarch64-linux.callPackage ./derivation.nix {};
+		packages.aarch64-linux.default = self.packages.aarch64-linux.mackeys;
 	};
 }
 
